706293f3918e99eb742f7ca8306764131e64e7d6
Reinhardt Erasmus <65610257+Spottie97@users.noreply.github.com>
2024-07-12 12:44:42 +0200

    "Updated backend to use default dotenv config and modified frontend Dockerfile to add WORKDIR, COPY, and EXPOSE instructions"

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` });
+require("dotenv").config();
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
