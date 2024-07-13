b0335cde51aab2bccba2b356c57a508e2f08ba3d
Reinhardt Erasmus <65610257+Spottie97@users.noreply.github.com>
2024-07-13 08:36:28 +0200

    Update packages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,13 +9,11 @@ function App() {
   const [selectedCompanies, setSelectedCompanies] = useState([]);
   const [industries, setIndustries] = useState([]);
 
-  const apiUrl = process.env.REACT_APP_API_URL;
-
   useEffect(() => {
     // Fetch industry data from backend
     const fetchIndustries = async () => {
       try {
-        const response = await fetch(`${apiUrl}/industries`);
+        const response = await fetch('/.netlify/functions/api/industries');
         const data = await response.json();
         
         // Check if the data is an array
@@ -30,11 +28,11 @@ function App() {
     };
 
     fetchIndustries();
-  }, [apiUrl]);
+  }, []);
 
   const handleSearch = async () => {
     try {
-      const response = await fetch(`${apiUrl}/search?location=${location}&industry=${industry}&radius=${radius}`);
+      const response = await fetch(`/.netlify/functions/api/search?location=${location}&industry=${industry}&radius=${radius}`);
       const data = await response.json();
 
       if (Array.isArray(data)) {
@@ -66,7 +64,7 @@ function App() {
 
   const handleDeleteSelected = async () => {
     try {
-      const response = await fetch(`${apiUrl}/delete`, {
+      const response = await fetch(`/.netlify/functions/api/delete`, {
         method: 'DELETE',
         headers: {
           'Content-Type': 'application/json'
